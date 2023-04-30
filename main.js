d1c41b8bd8c6891131b356163c79d5e64ca268bb
Sabrina Drashen <sdrashen@gmail.com>
2023-04-30 18:06:39 -0300

    uses template string

--- main.js
+++ main.js
@@ -7,16 +7,18 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 let cont = 0;
 
 while (cont < listaDeTeclas.length) {
+
     const tecla = listaDeTeclas[cont];
     const instrumento = tecla.classList[1];
 
-    console.log(instrumento);
+    const idAudio = `#som_${instrumento}`;
+    // console.log(idAudio);
 
     tecla.onclick = function () {
-        tocaSom('#som_tecla_pom');
+        tocaSom(idAudio);
     }
     cont++;
-    console.log(cont);
+    // console.log(cont);
 }
 
 //Para evitar repetição de código (cont), criamos uma const para a tecla.
