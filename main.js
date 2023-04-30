0cf04a85b71d37bb13b56fb2b1f482c53f9f72f0
Sabrina Drashen <sdrashen@gmail.com>
2023-04-30 17:59:51 -0300

    creates const tecla

--- main.js
+++ main.js
@@ -4,25 +4,19 @@ function tocaSom(idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-// listaDeTeclas[0].onclick = tocaSomPom;
-
 let cont = 0;
 
 while (cont < listaDeTeclas.length) {
-    const instrumento = listaDeTeclas[cont].classList[1];
+    const tecla = listaDeTeclas[cont];
+    const instrumento = tecla.classList[1];
 
     console.log(instrumento);
 
-    listaDeTeclas[cont].onclick = function () {
+    tecla.onclick = function () {
         tocaSom('#som_tecla_pom');
     }
     cont++;
     console.log(cont);
 }
 
-/*listaDeTeclas[cont].onclick = tocaSom(); se colocar com () vai dar o erro
-*"usuário ainda não interagiu com a página". Precisamos passar um parâmetro pra
-*o atributo onclick sem executá-lo imeditamente. Aí entra a arrow function.
-*As funções anônimas são utilizadas quando elas são o valor de algum atributo
-* ou estão sendo usadas dentro de uma variável.
-*/
+//Para evitar repetição de código (cont), criamos uma const para a tecla.
