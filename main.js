8055146966e243bd77a13f95bfb1bfd82ae37937
Sabrina Drashen <sdrashen@gmail.com>
2023-04-30 17:18:26 -0300

    uses while

--- main.js
+++ main.js
@@ -4,4 +4,12 @@ function tocaSomPom() {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
+listaDeTeclas[0].onclick = tocaSomPom;
 
+let cont = 0;
+
+while (cont < listaDeTeclas.length) {
+    listaDeTeclas[cont].onclick = tocaSomPom;
+    cont++;
+    console.log(cont);
+}
