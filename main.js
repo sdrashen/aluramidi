e8eec716241b8e0a9e2a33b4330af9bb194d04bf
Sabrina Drashen <sdrashen@gmail.com>
2023-04-30 17:51:33 -0300

    uses arrow function

--- main.js
+++ main.js
@@ -9,7 +9,16 @@ listaDeTeclas[0].onclick = tocaSomPom;
 let cont = 0;
 
 while (cont < listaDeTeclas.length) {
-    listaDeTeclas[cont].onclick = tocaSom;
+    listaDeTeclas[cont].onclick = function () {
+        tocaSom('#som_tecla_pom');
+    }
     cont++;
     console.log(cont);
 }
+
+/*listaDeTeclas[cont].onclick = tocaSom(); se colocar com () vai dar o erro
+*"usuário ainda não interagiu com a página". Precisamos passar um parâmetro pra
+*o atributo onclick sem executá-lo imeditamente. Aí entra a arrow function.
+*As funções anônimas são utilizadas quando elas são o valor de algum atributo
+* ou estão sendo usadas dentro de uma variável.
+*/
